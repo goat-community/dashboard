c0b4569f0bb1d9a484d12b762ab24300abeb557b
Sina Farhadi <sina.farhadi.cyber@gmail.com>
2022-07-07 19:38:20 +0430

    refactor: remove unused

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,19 +1,13 @@
 import { Route } from "react-router-dom";
 import { Admin, CustomRoutes, Resource } from "react-admin";
-import UserIcon from "@mui/icons-material/People";
 /** Pages */
 import LoginPage from "@pages/login/Login";
 /** Providers */
 import { i18nProvider } from "@utils";
-import jsonServerProvider from "ra-data-json-server";
 
 export default function AppRouter() {
   return (
-    <Admin
-      dataProvider={jsonServerProvider("https://jsonplaceholder.typicode.com")}
-      i18nProvider={i18nProvider}
-      requireAuth
-    >
+    <Admin i18nProvider={i18nProvider} requireAuth>
       <Resource name="users" />
       <CustomRoutes noLayout>
         <Route path="/panel" element={<LoginPage />} />
