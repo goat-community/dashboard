4ebda286028b6fec127a75abd9d5b82143c1c267
Sina Farhadi <sina.farhadi.cyber@gmail.com>
2022-07-07 19:37:26 +0430

    feat: draft text input component

---
+++ src/common/TextInput.tsx
@@ -0,0 +1,35 @@
+import { TextInput, required } from "react-admin";
+
+interface PTextInputProps {
+  source: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  autoFocus?: boolean;
+  fullWidth?: boolean;
+  validation?: any;
+}
+
+export function PTextInput(props: PTextInputProps) {
+  const {
+    source,
+    label,
+    type = "text",
+    disabled,
+    autoFocus,
+    fullWidth = false,
+    validation
+  } = props;
+
+  return (
+    <TextInput
+      autoFocus={autoFocus}
+      source={source}
+      label={label}
+      type={type}
+      disabled={disabled}
+      validate={validation}
+      fullWidth={fullWidth}
+    />
+  );
+}
