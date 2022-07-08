d2d44a3f7336616c30816f93224bdccf9232697a
Sina Farhadi <sina.farhadi.cyber@gmail.com>
2022-07-08 14:46:51 +0430

    fix: ignore typescript warns due to i18n

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,8 +1,9 @@
 import polyglotI18nProvider from "ra-i18n-polyglot";
 import en from "ra-language-english";
+// @ts-ignore
 import de from "ra-language-german";
 
-const translations = { en, de };
+const translations = { en, de } as { [key: string]: any };
 
 export const i18nProvider = polyglotI18nProvider(
   (locale) => translations[locale],
