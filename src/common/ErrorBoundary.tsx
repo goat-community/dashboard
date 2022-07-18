6140327ce04425d3667198dc56be1624e8d4be2c
Sina Farhadi <sina.farhadi.cyber@gmail.com>
2022-07-18 13:43:06 +0430

    fix: error boundary style

--- src/common/ErrorBoundary.tsx
+++ src/common/ErrorBoundary.tsx
@@ -6,16 +6,18 @@ interface Props {
 
 interface State {
   hasError: boolean;
+  error: Error | null;
 }
 
 export class ErrorBoundary extends Component<Props, State> {
   public state: State = {
-    hasError: false
+    hasError: false,
+    error: null
   };
 
   public static getDerivedStateFromError(_: Error): State {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { hasError: true, error: _ };
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
@@ -24,7 +26,12 @@ export class ErrorBoundary extends Component<Props, State> {
 
   public render() {
     if (this.state.hasError) {
-      return <h1>Sorry.. there was an error</h1>;
+      return (
+        <>
+          <h1>Sorry.. there was an error</h1>
+          <h3 className="error-text">{this.state.error!.toString()}</h3>
+        </>
+      );
     }
 
     return this.props.children;
--- src/resources/users/DatagridHeader.tsx
+++ src/resources/users/DatagridHeader.tsx
@@ -1,16 +1,19 @@
 import { TableHead, TableRow, TableCell } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import type { DatagridHeaderProps } from "react-admin";
 
-export function DatagridHeader(props: any) {
+export function DatagridHeader(props: DatagridHeaderProps) {
   return (
     <TableHead>
       <TableRow>
-        <TableCell></TableCell>{" "}
-        {/* empty cell to account for the select row checkbox in the body */}
-        {props.children.map((child: JSX.Element) => (
+        <TableCell></TableCell>
+        {/* @ts-ignore */}
+        {props.children!.map((child: JSX.Element) => (
           <TableCell
             key={child.props.source + child.props.label}
             sx={{ fontSize: 18, fontWeight: "bold" }}
           >
+            <DeleteIcon />
             {child.props.source || "Action"}
           </TableCell>
         ))}
