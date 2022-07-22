36d38146a30dae6eb1d1ba9da3d7ec939386d330
Sina Farhadi <sina.farhadi.cyber@gmail.com>
2022-07-22 11:28:42 +0430

    feat(draft): menu style changes

--- src/common/SideBar.tsx
+++ src/common/SideBar.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { Link, useTranslate } from "react-admin";
 import PeopleIcon from "@mui/icons-material/People";
 import LogoutIcon from "@mui/icons-material/Logout";
+import LayersIcon from "@mui/icons-material/Layers";
+import ListIcon from "@mui/icons-material/List";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
 import { logout } from "@context/user";
 import GoatLogo from "@assets/images/logo_green.webp";
 import { useAppDispatch } from "@hooks";
@@ -8,6 +12,7 @@ import { useAppDispatch } from "@hooks";
 export function Sidebar() {
   const translate = useTranslate();
   const dispatch = useAppDispatch();
+  const [collapsed, setCollapsed] = useState(false);
 
   return (
     <aside className="sidebar-container">
@@ -20,12 +25,24 @@ export function Sidebar() {
             Users
           </li>
         </Link>
-        <Link to={"/layers"}>
-          <li>
-            <PeopleIcon />
-            Layers
-          </li>
-        </Link>
+        <li onClick={() => setCollapsed((prevState) => !prevState)}>
+          <LayersIcon />
+          Layers
+        </li>
+        {collapsed && (
+          <div className="collapsed-menu">
+            <Link to="layers">
+              <li>
+                <ListIcon />
+                Styles
+              </li>
+            </Link>
+            <li>
+              <FileUploadIcon />
+              Upload
+            </li>
+          </div>
+        )}
         <li className="logout" onClick={() => dispatch(logout())}>
           <LogoutIcon />
           Logout
